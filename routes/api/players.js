e616a77df5a25cd16a6bcc682df15b0e2991baa5
Laurence Dale <laurencedale1986@gmail.com>
2020-04-10 17:33:45 +0100

    add player test route - no db connect

--- routes/api/players.js
+++ routes/api/players.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
+
 // @ route POST api/players
 // @ desc Test route (get player data to render into table)
 // @ Access - actually we wont have token based auth yet. Just basic auth
-router.post('/', 
+// For this post, we want to update scores, decks, avatar, idk. 
+
+router.put('/', 
 [
     check('name', 'Name is required')
     .not()
     .isEmpty(),
-    check('wins', 'Scores are required')
-    .not()
-    .isEmpty(),
-    check('losses', 'Scores are required')
-    .not()
-    .isEmpty(),
-    check('draws', 'Scores are required')
+    check('decklist', 'MTG Goldfish Tournament decklist is required')
     .not()
     .isEmpty()
-
 ], 
 (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     } else
-
-    res.send('Players Submitted!');
+    
+    res.send('Player Added!');
 });
 
 router.get('/',
