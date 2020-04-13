37167e060aaa146141569e0acba4cfc969990d6f
Laurence Dale <laurencedale1986@gmail.com>
2020-04-13 17:21:14 +0100

    up to 14 - login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,22 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Player = require('../../models/Player')
 
 // @ route GET api/auth
 // @ desc Test route (jwt authentication)
 // @ Access - public
-router.get('/', (req, res) => res.send('Auth route'));
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const player = await Player.findById(req.player.id).select('-password');
+    res.json(player);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  } 
+  
+});
 
 module.exports = router;
--- routes/api/players.js
+++ routes/api/players.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator');
 
 //Bring in player model
-const User = require('../../models/Player');
+const Player = require('../../models/Player');
 
-// @ route Put api/players
+// @ route POST api/players
 // @ desc       Register user (on second thought, maybe i dont want decklist here) 
-// @ Access - actually we wont have token based auth yet. Just basic auth
-// For this post, we want to update scores, decks, avatar, idk. 
+// @ Access - public - i guess? I mean, we probably need to provide a registration key to lock it down to only users we want
+// 
+//  function takes a username, password and decklist  
+// 
 
-router.put('/', 
+router.post('/', 
 [
     check('name', 'Name is required')
     .not()
@@ -24,29 +29,119 @@ router.put('/',
 ], 
 async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     } 
 
-    const {name, email, password} = req.body;
+    const {name, email, password, decklist} = req.body;
+
 
     try {
-        
+
+    // need to check if user is unique
+
+    let player = await Player.findOne({ name });
+    
+    if (player) {
+        res.status(400).json({errors: [{ msg: 'Player exists '}]});
+    }
+
+    // Create user
+
+    player = new Player({
+        name,
+        email,
+        password,
+        decklist
+    });
+
+    // encrypt password
+
+    const salt = await bcrypt.genSalt(10);
+
+    player.password = await bcrypt.hash(password, salt);
+
+    await player.save();
+
+    // return jwt
+
+    const payload = {
+        player: {
+            id: player.id
+        }
+    };
+
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        // this should be 3600 for an hour
+        { expiresIn: 360000 },
+        (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });        
+    
     } catch (err) {
-        
+        console.log(err.message);
+        res.status(500).send('Server Error');   
     }
     
 
-    res.send('Player Added!');
+
 });
 
 // @ route GET /api/players
+// @desc    Login User / Returning JWT Token
+// @access  Public
 
-
-router.get('/',
-(req, res) => {
-    res.send('Players Route');
+router.get('/', (req, res) => {
+    res.send('This is the players route')
 });
 
+// router.post('/login', (req, res) => {
+//     const { errors, isValid } = validateLoginInput(req.body);
+  
+//     // Check Validation
+//     if (!isValid) {
+//       return res.status(400).json(errors);
+//     }
+  
+//     const email = req.body.email;
+//     const password = req.body.password;
+  
+//     // Find user by email
+//     User.findOne({ email }).then(user => {
+//       // Check for user
+//       if (!user) {
+//         errors.email = 'User not found';
+//         return res.status(404).json(errors);
+//       }
+  
+//       // Check Password
+//       bcrypt.compare(password, user.password).then(isMatch => {
+//         if (isMatch) {
+//           // User Matched
+//           const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+  
+//           // Sign Token
+//           jwt.sign(
+//             payload,
+//             keys.secretOrKey,
+//             { expiresIn: 3600 },
+//             (err, token) => {
+//               res.json({
+//                 success: true,
+//                 token: 'Bearer ' + token
+//               });
+//             }
+//           );
+//         } else {
+//           errors.password = 'Password incorrect';
+//           return res.status(400).json(errors);
+//         }
+//       });
+//     });
+//   });
+  
 
 module.exports = router;
