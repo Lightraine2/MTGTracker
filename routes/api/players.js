7204b7a318e62d73184493ec57b2c1b4a16f65b4
Laurence Dale <laurencedale1986@gmail.com>
2020-04-13 11:03:14 +0100

    updates

--- routes/api/players.js
+++ routes/api/players.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 
+//Bring in player model
+const User = require('../../models/Player');
+
 // @ route Put api/players
 // @ desc       Register user (on second thought, maybe i dont want decklist here) 
 // @ Access - actually we wont have token based auth yet. Just basic auth
@@ -12,16 +15,28 @@ router.put('/',
     check('name', 'Name is required')
     .not()
     .isEmpty(),
+    check('password', 'Password is required')
+    .not()
+    .isEmpty(),
     check('decklist', 'MTG Goldfish Tournament decklist is required')
     .not()
     .isEmpty()
 ], 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
-    } else
+    } 
+
+    const {name, email, password} = req.body;
+
+    try {
+        
+    } catch (err) {
+        
+    }
     
+
     res.send('Player Added!');
 });
 
