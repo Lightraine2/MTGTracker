d68609da82a2e221a15f491443c8fe0260a78a48
Laurence Dale <laurencedale1986@gmail.com>
2020-04-14 21:50:42 +0100

    starting with landing page and Navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+      <h1>
+        <a href="index.html"><i className="fab fa-wizards-of-the-coast"></i> MTG Tournament Tracker</a>
+      </h1>
+      <ul>
+        <li><a href="profiles.html">Tournaments</a></li>
+        <li><a href="register.html">Register</a></li>
+        <li><a href="login.html">Login</a></li>
+      </ul>
+    </nav>
+    )
+}
+
+export default Navbar;
