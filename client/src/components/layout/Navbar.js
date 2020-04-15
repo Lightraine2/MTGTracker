2b1ee4d45b3268ddf937c602f06785042e3d2e48
Laurence Dale <laurencedale1986@gmail.com>
2020-04-15 14:20:45 +0100

    33 react router set up

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fab fa-wizards-of-the-coast"></i> MTG Tournament Tracker</a>
+        <Link to="/"><i className="fab fa-wizards-of-the-coast"></i> MTG Tournament Tracker</Link>
       </h1>
       <ul>
-        <li><a href="profiles.html">Tournaments</a></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
     )
