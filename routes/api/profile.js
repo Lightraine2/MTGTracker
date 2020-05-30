8f6d2982b9b9a3d815ecb5fe845ae1377288b820
Laurence Dale <laurencedale1986@gmail.com>
2020-05-30 10:23:36 +0100

    delete session

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -204,6 +204,27 @@ router.put('/sessions', [auth, [
 
 );
 
+// @route DELETE api/profile/sessions/:session_id
+
+router.delete('/sessions/:session_id', auth, async (req, res) => {
+    try {
+       const profile = await Profile.findOne({ user: req.user.id});
+       
+       // Get remote index
+       const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.session_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
 
 module.exports = router;
 
