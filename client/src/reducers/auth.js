df2e788c2c082846ddfde216f5d95f6d1a6c8eef
Lightraine2 <laurencedale1986@gmail.com>
2020-06-01 21:02:22 +0100

    log out and navbar update

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -14,10 +15,10 @@ const initialState = {
     user: null
 }
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     const { type, payload } = action;
 
-    switch(type) {
+    switch (type) {
         case USER_LOADED:
             return {
                 ...state,
@@ -45,28 +46,35 @@ export default function(state = initialState, action) {
         case REGISTER_FAIL:
             localStorage.removeItem('token');
             return {
-               ...state,
-               token: null,
-               isAuthenticated: false,
-               loading: false
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
             };
         case LOGIN_FAIL:
             localStorage.removeItem('token');
             return {
-               ...state,
-               token: null,
-               isAuthenticated: false,
-               loading: false
-            };    
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
         case AUTH_ERROR:
             localStorage.removeItem('token');
             return {
-               ...state,
-               token: null,
-               isAuthenticated: false,
-               loading: false
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
             };
-
+        case LOGOUT:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
         default:
             return state;
     }
